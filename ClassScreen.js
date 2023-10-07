07f5cf00a874f2ef293b70cc9ac29a58b8417464
TOOm <97885350+TomVoituret@users.noreply.github.com>
2023-10-07 18:48:13 +0200

    fonctionnement du serveur

--- ClassScreen.js
+++ ClassScreen.js
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
 
 const ClasseScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [text, setText] = useState('');
-  const [openScanner, setOpenScanner] = useState(false); // Ajouté pour contrôler l'affichage du scanner
+  const [openScanner, setOpenScanner] = useState(false);
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://192.168.1.24:3000/recuperer_calendrier', {
+        params: {
+          url: text
+        }
+      });
+      setData(response.data);
+
+      // Sauvegarder les données dans AsyncStorage
+      await AsyncStorage.setItem('calendarData', JSON.stringify(response.data));
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données:', error);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -17,8 +36,8 @@ const ClasseScreen = () => {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    setOpenScanner(false); // Fermer le scanner une fois un code scanné
-    setText(data); // Mettre à jour l'input avec la donnée scannée
+    setOpenScanner(false);
+    setText(data);
     alert(`QR Code scanné ! Donnée: ${data}`);
   };
 
@@ -35,7 +54,7 @@ const ClasseScreen = () => {
         style={styles.input}
         placeholder="Entrer la classe"
         value={text}
-        onChangeText={setText}
+        onChangeText={(text) => setText(text)}
       />
       <View style={styles.buttonContainer}>
         <Button title="Ouvrir le Scanner" onPress={() => setOpenScanner(true)} />
@@ -47,8 +66,10 @@ const ClasseScreen = () => {
         />
       )}
       <View style={styles.buttonContainer}>
-        <Button title="Valider" />
+        <Button title="Valider" onPress={fetchData} />
       </View>
+      <Text>Résultat de la récupération :</Text>
+      <Text>{data ? JSON.stringify(data) : 'Chargement en cours...'}</Text>
     </View>
   );
 };
@@ -67,7 +88,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   buttonContainer: {
-    marginBottom: 10, // Cela ajoutera un espace en dessous du bouton.
+    marginBottom: 10,
   },
 });
 
